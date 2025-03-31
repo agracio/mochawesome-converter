8dd47460720cf546b704e6fc0100e7ffb33c5837
agracio <agracio@users.noreply.github.com>
2025-03-31 18:33:43 +0100

    adding bin

---
+++ bin/cli.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs');
+const { yargsOptions } = require('../src/options');
+const { config } = require('../src/config');
+const convert = require('../src/converter');
+
+yargs
+    .usage('Usage: $0 [options]')
+    .example('$0 --testFile mytesfiles/nunit.xml --testType nunit', '')
+    .demandOption(['testFile','testType'])
+    .options(yargsOptions)
+    .help('help')
+    .alias('h', 'help')
+    .version().argv;
+
+const options = (config(yargs.argv));
+convert(yargs.argv).then(() => console.log(`Report created at '${options.reportDir}/'`));
+
---
+++ src/options.js
@@ -0,0 +1,52 @@
+export const yargsOptions = {
+    testFile: {
+        describe: 'Path to test file',
+        string: true,
+        default: undefined,
+    },
+    testType: {
+        describe: 'Test type',
+        string: true,
+        default: undefined,
+    },
+    reportDir: {
+        default: './report',
+        describe: 'Report output directory',
+        string: true,
+    },
+    reportFilename: {
+        default: undefined,
+        describe: 'Mochawesome report name',
+        string: true,
+    },
+    junit: {
+        default: false,
+        describe: 'Create JUnit XML report?',
+        boolean: true,
+    },
+    junitReportFilename: {
+        describe: 'JUnit report name',
+        string: true,
+        default: undefined,
+    },
+    html: {
+        default: false,
+        describe: 'Create Mochawesome HTML?',
+        boolean: true,
+    },
+    htmlReportFilename: {
+        default: undefined,
+        describe: 'Mochawesome HTML file name',
+        string: true,
+    },
+    skippedAsPending: {
+        default: true,
+        describe: 'Show skipped tests as pending in Mochawesome report',
+        boolean: true,
+    },
+    switchClassnameAndName: {
+        default: false,
+        describe: 'Switch test case classname and name',
+        boolean: true,
+    },
+};
