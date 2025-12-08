b598a473b26f7dbf0f3d138b53b59d9c1b607368
agracio <agracio@users.noreply.github.com>
2025-12-08 15:35:50 +0000

    updating jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,14 @@
 module.exports = {
     verbose: true,
     globalTeardown: "./tests/teardown.js",
+    preset: "ts-jest",
     reporters: [
         'default',
         ['github-actions', {silent: false}], 
         'summary',
         ['jest-junit', { suiteName: "mochawesome-converter tests" }]
-        // ["jest-html-reporters", {
-        //     publicPath: './tests/report',
-        //     filename: 'report.html',
-        //     darkTheme: true,
-        //     pageTitle: 'mochawesome-converter',
-        //     expand: true,
-        //     urlForTestFiles: 'https://github.com/agracio/mochawesome-converter/blob/main'
-        //   }
-        // ]
     ],
-    "collectCoverageFrom": [
-        "src/**/*.js"
-    ],
+    testMatch: ["<rootDir>/tests/**/*.test.{js,jsx,ts,tsx}"],
+    collectCoverage: true,
+    coverageDirectory: "coverage",
 }
